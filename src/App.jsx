47911ef52b1abf0098e8a864670d1b34e961ea31
Geovane Alonso <geovane.halonso.2005@gmail.com>
2025-09-26 21:16:20 -0300

    26-09-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Hello } from "./components/hello";
+import { Card } from "./components/Card";
+
+const CARDS = [
+  {id:0, title: 'Card A', description: 'Filho de A' },
+  {id:1, title: 'Card B', description: 'Filho de B' },
+  {id:2, title: 'Card C', description: 'Filho de C' },
+]
+
 const App = () => {
   return (
     <main className="min-h-dvh bg-slate-50">
@@ -5,19 +14,30 @@ const App = () => {
         Hello Tailwind + React!
       </h1>
       <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <div className="rounded-2xl border p-4 bg bg-slate-400 text-neutral-50 shadow-sm">
-          Card A
-        </div>
-        <div className="rounded-2xl border p-4 bg bg-slate-400 text-neutral-50 shadow-sm">
-          Card B
-        </div>
-        <div className="rounded-2xl border p-4 bg bg-slate-400 text-neutral-50 shadow-sm">
-          Card C
-        </div>
+       {/* <Card title="Card A">
+          <p>Eu sou o filho de A</p>
+          </Card>
+
+          <Card title="Card B">
+            <p>Eu sou o filho de B</p>
+          </Card>
+          
+          <Card title="Card C">
+            <p>Eu sou o filho de B</p>
+          </Card>  */}
+
+          {CARDS.map(item => (
+            <Card key={item.id} title={item.title}>
+              <p>{item.description}</p>
+            </Card>
+          ))}
+
       </div>
+      
       <a href="#" className="inline-block px-4 py-2 rounded-lg border hover:bg-slate100 focus:ring-2 focust:ring-blue-500">
         Link com foco
       </a>
+      <Hello name="Geovane" />
     </main>
   );
 }
---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,3 @@
+export const Hello = ({name = "Mentes Brilhantes"}) => {
+    return <h2 className="text-xl">Olá, {name} 👌</h2>
+}
